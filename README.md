0e8fcc2c6849cec575c06a0682fe1e193fdbd7a5
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2018-02-22 22:12:24 -0800

    Add bigquery-to-datastore.rb

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # homebrew-bigquery-to-datastore
-A homebrew tap for bigquery-to-datastore
+
+This repository is used for install [yu-iskw/bigquery-to-datastore](https://github.com/yu-iskw/bigquery-to-datastore) with [Homebrew](https://docs.brew.sh/).
+
+## Requirements
+- [Homebrew](https://docs.brew.sh/).
+- Java 1.8+
+
+## How to install
+
+If you would like to know the detail of command line options, please take a look at [yu-iskw/bigquery-to-datastore](https://github.com/yu-iskw/bigquery-to-datastore).
+
+```
+# install
+brew install yu-iskw/bigquery-to-datastore/bigquery-to-datastore
+
+# show help
+bigquery-to-datastore --help
+```
+
+## License
+
+Copyright (c) 2017 Yu Ishikawa.
---
+++ bigquery-to-datastore.rb
@@ -0,0 +1,38 @@
+##
+## License
+##
+## Copyright (c) 2017 Yu Ishikawa.
+##
+
+class BigqueryToDatastore < Formula
+  desc "Export a whole BigQuery table to Google Datastore with Apache Beam/Google Dataflow"
+  homepage "https://github.com/yu-iskw/bigquery-to-datastore"
+  url "https://github.com/yu-iskw/bigquery-to-datastore/archive/0.4.1.tar.gz"
+  sha256 "d3a03f071c72ab18ea0fd71a57d92b2afb647854ccc6bfbfa929a84797067d14"
+
+  depends_on :java
+  depends_on "maven"
+
+  def install
+    # build
+    system "make", "package" 
+
+    # install
+    libexec.install Dir["*"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bigquery-to-datastore`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
